3bb90ab012abacfc8edd3332d2ba8fee7b2e573d
Preethi Pattabiraman <preetiauguststar@gmail.com>
2021-04-03 10:15:34 +0530

    feat : Basic BananaSpeak

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # neoGCamp-lesson-four
  neoGCamp-lesson-four - bananaSpeak
+
+ codepens for QuerySelectors
+
+ https://codepen.io/preethi-pattabiraman/pen/NWdpeKP?editors=1111
---
+++ index.js
@@ -0,0 +1,7 @@
+var buttonTranslate = document.querySelector("#btn-translate");
+var textarea = document.querySelector("#txt-input");
+
+buttonTranslate.addEventListener("click", () => {
+    var input = textarea.value;
+});
+
