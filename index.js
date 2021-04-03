f5cd8e0ba333b8abccff1119022240d97d73d71e
Preethi Pattabiraman <preetiauguststar@gmail.com>
2021-04-03 10:33:54 +0530

    feat : Output

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var buttonTranslate = document.querySelector("#btn-translate");
 var textarea = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#div-output");
 
 buttonTranslate.addEventListener("click", () => {
     var input = textarea.value;
+    outputDiv.innerText = "HHHHHH " + input;
 });
 
